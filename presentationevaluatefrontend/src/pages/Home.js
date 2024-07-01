09e985701560d34e82dc4ec265f1eb141b8e532c
Rakshit Ranpariya <rakshitranpariya@gmail.com>
2024-07-01 08:07:37 -0300

    Updated Frontend: Main.js

--- presentationevaluatefrontend/src/pages/Home.js
+++ presentationevaluatefrontend/src/pages/Home.js
@@ -1,17 +1,34 @@
 import React from "react";
 import "./Home.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from '../images/logo.png'; // Adjust the path according to your project structure
+import logo from '../images/pewhite.png'; // Adjust the path according to your project structure
 
 const Home = () => {
     return (
-        <div>
+        <div class="HomeBody">
             <div className="header">
-                <img src={logo} className="Logo" alt="Logo"></img>
-                <p className="LogoTitle"> presentrationelvcatu</p>
+                <div className="Logo">
+                    <img src={logo} className="LogoImage" alt="Logo"></img>
+                    <p className="LogoTitle h3 text-black fw-bold"> Presentation Evaluator </p>
                 </div>
-                <button className="primaryButton"></button>
-                <button className="LoginIn"></button>
+                <div class="LeftOptions">
+                    <p class="MenuOption">Explore</p>
+                    <p class="MenuOption">About Us</p>
+                    <p class="MenuOption"> Contact Us </p>
+                </div>
+            </div>
+            <div className="MainBanner">
+                <div class="ButtonDiv">
+                    <button className="btn btn-dark BannerButtons ">Start Evaluating</button>
+                    <button className="btn btn-outline-dark BannerButtons"> About Us</button>
+                </div>
+            </div>
+                
+            <div class="Footer">
+                    <p class="FooterText">All rights reserved.</p>
+            </div>
+             
+
         </div>
         
     );
--- presentationevaluatefrontend/src/pages/Main.js
+++ presentationevaluatefrontend/src/pages/Main.js
@@ -1,44 +1,71 @@
 import { useState } from "react";
+import "./Main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../images/pewhite.png"; // Adjust the path according to your project structure
 const Main = () => {
-    const [file, setFile] = useState(null);
+  const [file, setFile] = useState(null);
 
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);  
-    };
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();  
-        if (file) {
-            const formData = new FormData();
-            formData.append('file', file);  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
 
-            try {
-                const response = await fetch('http://localhost:5000/upload', {
-                    method: 'POST',
-                    body: formData,
-                    enctype:"multipart/form-data"
-                });
+      try {
+        const response = await fetch("http://localhost:5000/upload", {
+          method: "POST",
+          body: formData,
+          enctype: "multipart/form-data",
+        });
 
-                const result = await response.json();
-                console.log('Success:', result);
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        } else {
-            console.log('No file selected');
-        }
-    };
+        const result = await response.json();
+        console.log("Success:", result);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    } else {
+      console.log("No file selected");
+    }
+  };
 
-    return (
-        <div>
-            <h1>Hey This is Main Page</h1>
-            <form onSubmit={handleSubmit}>
-                <h1>React File Upload</h1>
-                <input type="file" onChange={handleFileChange} />
-                <button type="submit" class="btn btn-primary">Upload</button>
-            </form>
-        </div>
+  return (
+    <div className="MainBody">
+    
+            <header>
+              <div className="Logo">
+                <img src={logo} className="LogoImage" alt="Logo"></img>
+                <p className="LogoTitle h3 text-black fw-bold">
+                  {" "}
+                  Presentation Evaluator{" "}
+                </p>
+              </div>
+              <div class="MenuOptionGroup">
+                <p class="MenuOption">Explore</p>
+                <p class="MenuOption">About Us</p>
+                <p class="MenuOption"> Contact Us </p>
+              </div>
+            </header>
+         
        
-    );
-}
+            <main>
+              <form onSubmit={handleSubmit}>
+                <h1 class="Heading">React File Upload</h1>
+                <input type="file" onChange={handleFileChange} />
+                <button type="submit" class="btn btn-primary">
+                  Upload
+                </button>
+              </form>
+            </main>
+         
+            <footer>
+              <p class="FooterText">All rights reserved.</p>
+            </footer>
+         
+    </div>
+  );
+};
 export default Main;
